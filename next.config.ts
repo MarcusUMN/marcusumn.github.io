bb1eb6d6ea5d1095e891e099f4b70e964f466276
Marcus <marcustaugner@outlook.com>
2025-07-02 18:08:02 -0500

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',       
-  trailingSlash: true, 
-  basePath: '',        
-  assetPrefix: './',
 }
 
 module.exports = nextConfig;
